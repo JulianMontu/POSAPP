2be124d890f5176cd590608a713848f82e957cbd
JulianMontu <julamontufar@misena.edu.co>
2023-05-02 20:22:41 -0500

    FirstCommit

---
+++ POS.Infrastructure/Persistence/Contexts/Configurations/ProvinceConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using POS.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.Infrastructure.Persistence.Contexts.Configurations
+{
+    public class ProvinceConfiguration : IEntityTypeConfiguration<Province>
+    {
+        public void Configure(EntityTypeBuilder<Province> builder)
+        {
+            builder.HasKey(e => e.ProvinceId).HasName("PK__Province__FD0A6F83C9D329F6");
+
+            builder.Property(e => e.Name)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+
+            builder.HasOne(d => d.Department).WithMany(p => p.Provinces)
+                .HasForeignKey(d => d.DepartmentId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Provinces__Depar__52593CB8");
+        }
+    }
+}
