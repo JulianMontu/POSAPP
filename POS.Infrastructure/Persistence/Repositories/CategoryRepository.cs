238728d1d807aa58d7d39ac2ff740c8f473002e8
JulianMontu <julamontufar@misena.edu.co>
2023-05-18 22:31:08 -0500

    Patron repositorio y patron unit of work

---
+++ POS.Infrastructure/Persistence/Repositories/CategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using POS.Domain.Entities;
+using POS.Infrastructure.Persistence.Contexts;
+using POS.Infrastructure.Persistence.Interfaces;
+
+namespace POS.Infrastructure.Persistence.Repositories
+{
+    public class CategoryRepository : GenericRepository<Category>, ICategoryRepository
+    {
+        private readonly PosContext _context;
+
+        public CategoryRepository(PosContext context)
+        {
+            _context = context;
+        }
+    }
+}
