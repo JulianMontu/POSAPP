b28cf581d8725552bc0c27656b11d2adb00ac483
Julian Montufar <89953374+JulianMontu@users.noreply.github.com>
2023-05-22 09:14:38 -0500

    Injection extensions

--- POS.Infrastructure/Extensions/InjectionExtensions.cs
+++ POS.Infrastructure/Extensions/InjectionExtensions.cs
@@ -2,6 +2,8 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using POS.Infrastructure.Persistence.Contexts;
+using POS.Infrastructure.Persistence.Interfaces;
+using POS.Infrastructure.Persistence.Repositories;
 
 namespace POS.Infrastructure.Extensions
 {
@@ -14,6 +16,7 @@ namespace POS.Infrastructure.Extensions
                 options => options.UseSqlServer(
                     configuration.GetConnectionString("POSConnection"), b => b.MigrationsAssembly(assembly)), ServiceLifetime.Transient);
 
+            services.AddTransient<IUnitsOfWork, UnitOfWork>(); 
             return services;
         }
     }
